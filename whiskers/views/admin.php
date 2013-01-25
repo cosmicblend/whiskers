63f2a9ef3b31260e81975702dc6c9bf5196d8121
cosmicblend <chet@cosmicblend.ca>
2013-01-24 17:42:33 -0800

    fixing my failure to update

--- whiskers/views/admin.php
+++ whiskers/views/admin.php
@@ -1,7 +1,6 @@
 <h2>Accounts</h2>
 <?php //print_r($available_drivers); ?>
 <form id="add_service" action="<?php echo site_url("admin/account_connect"); ?>" method="post">
-<<<<<<< HEAD
 	<p>Add an account for</p>
 		
 	<select name="add[driver]">
@@ -9,31 +8,17 @@
 		<option value="<?php echo $driver ?>"><?php echo ucwords($driver) ?></option>
 	<?php endforeach; endif; ?>
 	</select>
-=======
-    <p>Add an account for</p>
-
-    <select name="add[driver]">
-    <?php if (isset($available_drivers)) : foreach ($available_drivers as $driver) : ?>
-        <option value="<?php echo $driver ?>"><?php echo ucwords($driver) ?></option>
-    <?php endforeach; endif; ?>
-    </select>
->>>>>>> upstream/master
 
     <input type="submit" value="Add" />
 </form>
 
 <p>Depending on the account, you'll be asked for a user name and password, or sent to the account's website to sign in and authorize <em>Whiskers App</em>.</p>
 <?php //print_r($valid_drivers); //valid = running ?>
-<<<<<<< HEAD
 
 <h2>Current Accounts</h2>
-=======
-<h3>Current Accounts</h3>
->>>>>>> upstream/master
 <?php if ($valid_drivers) : ?>
 	<ul class="accounts-list">
 <?php foreach ($valid_drivers as $driver => $obj): ?>
-<<<<<<< HEAD
 		 <?php if('twitter' === $driver) : ?>
 		<li class="row">
 			<div>
@@ -63,30 +48,6 @@
 				<input id="remove_service_submit" onclick="document.getElementById('face').className = 'chesire'" name="rm[op]" type="submit" value="Remove" />
 			</form>
 		</li>
-=======
-
-    <?php if('twitter' === $driver) : ?>
-        <h4><?php echo ucwords($driver) ?></h4> 
-        <a href="http://twitter.com/<?php echo $obj->access_token->screen_name ?>"><?php echo $obj->access_token->screen_name ?></a>
-    <?php endif; ?>
-
-    <?php if('facebook' === $driver) : ?>
-        <h4><?php echo ucwords($driver) ?></h4>
-        <a href="<?php echo $obj->user->link ?>"><?php echo $obj->user->name ?></a>
-    <?php endif; ?>
-
-    <?php if('appnet' === $driver) : ?>
-        <h4><?php echo ucwords($driver) ?></h4>
-        <a href="https://alpha.app.net/<?php echo $obj->user->nickname ?>"><?php echo $obj->user->name ?></a>
-    <?php endif; ?>
-
-    <form id="remove_service" action="<?php echo site_url('admin'); ?>" method="post" style="float:right">
-    <input type="hidden" name="rm[driver]" value="<?php echo $driver; ?>" />
-    <input id="remove_service_submit" onclick="document.getElementById('face').className = 'chesire'" name="rm[op]" type="submit" value="Remove" />
-    </form>
-
-    <hr>
->>>>>>> upstream/master
 <?php endforeach; ?>
 	</ul>
 <?php else: ?>
--- whiskers/views/admin/appnet_app.php
+++ whiskers/views/admin/appnet_app.php
@@ -1,5 +1,4 @@
 <h2>App.net App</h2>
-<<<<<<< HEAD
 <form name="appnet_app" action="<?php echo site_url("admin/appnet_app"); ?>" method="post">
 	<div class="messages">
 	<p>You need to create a App.net app and add the details here to continue:</p>
@@ -29,38 +28,4 @@
 	</div>
 	
 	<input type="submit" value="Save details" />
-=======
-
-<form name="appnet_app" action="<?php echo site_url("admin/appnet_app"); ?>" method="post">
-
-    <p>You need to create a App.net app and add the details here to continue:</p>
-
-    <ul>
-        <li>Go to <a target="_blank" href="http://app.net">http://app.net</a></li>
-        <li>Sign in with your App.net account if you're not already</li>
-        <li>Click on your avatar in the top-right and select "My Apps".</li>
-        <li>Click "Create An App".</li>
-        <li>Fill in the Name field with something like "John's Whiskers"</li>
-        <li>Add a website url (the current url if you'd like).</li>
-        <li>Enter your callback url: <strong><?php print site_url('admin/appnet_connect'); ?></strong></li>
-        <li>Save the app and copy the "Client ID" and "Client Secret" to Whiskers. You should be good to go!</li>
-    </ul>
-
-    <p>Add your App.net details:</p>
-
-    <hr />
-
-    <p>
-    <label for="appnet_consumer_key">Consumer Key</label>
-    <input name="appnet_consumer_key" type="text" value="<?php echo $old_consumer_key ?>">
-    </p>
-
-    <p>
-    <label for="appnet_consumer_secret">Consumer Secret</label>
-    <input name="appnet_consumer_secret" type="text" value="<?php echo $old_consumer_secret ?>">
-    </p>
-
-    <input type="submit" value="Save details" />
-
->>>>>>> upstream/master
 </form>
