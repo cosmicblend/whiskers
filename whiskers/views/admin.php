ce6baa86dff9c264e923a6f5a25e979fb5dd06de
cosmicblend <chet@cosmicblend.ca>
2013-01-24 17:06:29 -0800

    mobile views. sign in, admin, service additions

--- whiskers/views/admin.php
+++ whiskers/views/admin.php
@@ -1,42 +1,55 @@
 <h2>Accounts</h2>
-
+<?php //print_r($available_drivers); ?>
 <form id="add_service" action="<?php echo site_url("admin/account_connect"); ?>" method="post">
-    <p>Add an a account for</p>
-
-    <select name="add[driver]">
-    <?php if (isset($available_drivers)) : foreach ($available_drivers as $driver) : ?>
-        <option value="<?php echo $driver ?>"><?php echo ucwords($driver) ?></option>
-    <?php endforeach; endif; ?>
-    </select>
+	<p>Add an account for</p>
+		
+	<select name="add[driver]">
+	<?php if (isset($available_drivers)) : foreach ($available_drivers as $driver) : ?>
+		<option value="<?php echo $driver ?>"><?php echo ucwords($driver) ?></option>
+	<?php endforeach; endif; ?>
+	</select>
 
     <input type="submit" value="Add" />
-
 </form>
 
 <p>Depending on the account, you'll be asked for a user name and password, or sent to the account's website to sign in and authorize <em>Whiskers App</em>.</p>
+<?php //print_r($valid_drivers); //valid = running ?>
 
-<h3>Current Accounts</h3>
+<h2>Current Accounts</h2>
 <?php if ($valid_drivers) : ?>
+	<ul class="accounts-list">
 <?php foreach ($valid_drivers as $driver => $obj): ?>
-
-    <?php if('twitter' === $driver) : ?>
-        <h4><?php echo ucwords($driver) ?></h4> 
-        <a href="http://twitter.com/<?php echo $obj->access_token->screen_name ?>"><?php echo $obj->access_token->screen_name ?></a>
-    <?php endif; ?>
-
-    <?php if('facebook' === $driver) : ?>
-        <h4><?php echo ucwords($driver) ?></h4>
-        <a href="<?php echo $obj->user->link ?>"><?php echo $obj->user->name ?></a>
-    <?php endif; ?>
-
-
-    <form id="remove_service" action="<?php echo site_url('admin'); ?>" method="post" style="float:right">
-    <input type="hidden" name="rm[driver]" value="<?php echo $driver; ?>" />
-    <input id="remove_service_submit" onclick="document.getElementById('face').className = 'chesire'" name="rm[op]" type="submit" value="Remove" />
-    </form>
-
-    <hr>
+		 <?php if('twitter' === $driver) : ?>
+		<li class="row">
+			<div>
+				<h3><?php echo ucwords($driver) ?></h3>
+				<a href="http://twitter.com/<?php echo $obj->access_token->screen_name ?>"><?php echo $obj->access_token->screen_name ?></a>
+			</div>
+		<?php endif; ?>
+		
+		<?php if('facebook' === $driver) : ?>
+		<li class="row">
+		     <div>
+				<h3><?php echo ucwords($driver) ?></h3>
+				<a href="<?php echo $obj->user->link ?>"><?php echo $obj->user->name ?></a>
+			</div>
+		<?php endif; ?>
+		
+		<?php if('appnet' === $driver) : ?>
+		<li class="row">
+			<div>
+				<h3><?php echo ucwords($driver) ?></h3>
+				<a href="https://alpha.app.net/<?php echo $obj->user->nickname ?>"><?php echo $obj->user->name ?></a>
+			</div>
+		<?php endif; ?>
+	
+			<form id="remove-service" action="<?php echo site_url('admin'); ?>" method="post">
+				<input type="hidden" name="rm[driver]" value="<?php echo $driver; ?>" />
+				<input id="remove_service_submit" onclick="document.getElementById('face').className = 'chesire'" name="rm[op]" type="submit" value="Remove" />
+			</form>
+		</li>
 <?php endforeach; ?>
+	</ul>
 <?php else: ?>
     <p>None.</p>
 <?php endif; ?>
--- whiskers/views/login.php
+++ whiskers/views/login.php
@@ -1,13 +1,11 @@
 <form id="post" action="<?php echo $action_url ?>" method="post">
-<div>
-  <label for="username">Username</label>
-  <input type="text" name="login[username]"  maxlength="50" value="" id="username">
-</div>
-<div class="form-item">
-  <label for="password">Password</label>
-  <input type="password" name="login[password]" value="" id="password">
-</div>
-<div class="form-item">
-  <input id="post-form-submit" onclick="document.getElementById('face').className = 'chesire'" name="op" type="submit" value="Login" />
-</div>
+	<div class="form-item">
+		<label for="username" class="is-hidden">Username</label>
+		<input type="text" name="login[username]" value="" placeholder="Your username" id="username">
+	</div>
+	<div class="form-item">
+		<label for="password" class="is-hidden">Password</label>
+		<input type="password" name="login[password]" value="" placeholder="Password" id="password">
+	</div>
+	<input id="post-form-submit" onclick="document.getElementById('face').className = 'chesire'" name="op" type="submit" value="Login" />
 </form>
